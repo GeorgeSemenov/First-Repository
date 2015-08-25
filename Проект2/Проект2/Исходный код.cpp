63f1e97937ed443d6c4386297fa636f2eeb50bd5
GeorgeSemenov <zhora.sem@yandex.ru>
2015-08-25 16:10:57 +0300

    One more konservative.

--- Проект2/Проект2/Исходный код.cpp
+++ Проект2/Проект2/Исходный код.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	std::cout << "hello worldds";
 	std::cout << "\nKonservative feature on C++";
+	std::cout << "Konservatism never dead!!! Heavy Metal!!!";
 	system("pause >> void ");
 	return 0;
 }
