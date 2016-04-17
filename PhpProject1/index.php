e072e4a02968bd95c81bbeef37f35fd13829ff3c
GeorgeSemenov <zhora.sem@yandex.ru>
2016-04-17 11:01:36 +0300

    Добавил проект по php для нетбинса

---
+++ PhpProject1/index.php
@@ -0,0 +1,37 @@
+<?php
+/*echo mb_strlen($string,'UTF-8');//Подсчитаем длину строки
+
+//Мастерский переворот строки!!!
+$str = '123разДваТри\?';
+$invertStr = iconv('utf-8', 'utf-16le',$str);
+$invertStr = strrev ($invertStr);
+$invertStr = iconv('utf-16be', 'utf-8',$invertStr);
+echo $invertStr;
+//Инфа отсюда : http://bolknote.ru/2012/04/02/~3625#56
+ 
+
+//Сравнение строk
+$a = 'Ха хашка';
+$b = 'хА хАшка';
+if(mb_strtolower($a,'UTF-8') == mb_strtolower($b,'UTF-8'))
+{echo "Строки совпадают";}
+else {echo "Строки не совпадают";}*/
+
+//убираем пробел из строки.
+/*$str = "Пирчет мойя звать ЖИРИ жирный ыыыы  ";
+$strNonSpaces = str_replace(" ","",$str);
+echo $strNonSpaces;*/
+
+//Копируем часть строки в другую переменную
+$str = "Креветки плывут в рот";
+$cutStr = mb_substr($str,5,5,'UTF-8');
+echo $cutStr;
+
+//Теперь пора браться за саму работу.
+
+$str = "Суда вводиться строка для проверки на палиндром";
+
+function search_palindrom ($str){
+    echo "привет , как дела";
+}
+?>
